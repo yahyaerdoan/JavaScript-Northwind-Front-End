7dae47a3ef4c3cc873ed235ac04d0751ce046b22
yahyaerdoan <77584301+yahyaerdoan@users.noreply.github.com>
2022-02-01 14:24:38 +0300

    Date yapısı çalışıldı.

---
+++ src/date.js
@@ -0,0 +1,11 @@
+const date = new Date();
+const nowdate = Date.now();
+console.log(date.getFullYear());
+console.log(date.getMonth());
+console.log(date.getDay());
+console.log(date.getHours());
+console.log(date.getMinutes());
+console.log(date.getMilliseconds());
+console.log(date.getTime());
+
+//tirhd party : moment.js pakati kullanılabilir.
